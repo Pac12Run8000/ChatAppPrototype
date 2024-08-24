2d65b708974c9998b00ca0a9c5bcf5368aeb3990
Norbert Grover <norbert298@gmail.com>
2024-08-23 18:34:22 -0700

    Initial Commit

---
+++ ChatAppPrototype/ChatAppPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppPrototypeApp.swift
+//  ChatAppPrototype
+//
+//  Created by Norbert Grover on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
