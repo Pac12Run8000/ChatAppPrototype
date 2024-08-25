dd23dee5624508aae1f0c55446a9bd83d9b07df2
Norbert Grover <norbert298@gmail.com>
2024-08-25 16:02:47 -0700

    Added styles to the chat tabbar

--- ChatAppPrototype/Views/MainTabView.swift
+++ ChatAppPrototype/Views/MainTabView.swift
@@ -1,8 +1,13 @@
 import SwiftUI
 
 struct MainTabView: View {
-
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(red: 26/255, green: 27/255, blue: 45/255, alpha: 1.0) // #1A1B2D
+        UITabBar.appearance().unselectedItemTintColor = UIColor(red: 91/255, green: 99/255, blue: 122/255, alpha: 1.0) // #5B637A
+    }
+   
     var body: some View {
+        
         TabView {
             ChatRouter.createChatModule()
                 .tabItem {
